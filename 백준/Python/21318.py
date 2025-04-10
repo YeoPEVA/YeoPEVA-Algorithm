87c4b54ac13a4da69a5a0f6c27e6a81a4d4b4486
YeoPEVA <lcy606@naver.com>
2025-04-10 16:49:56 +0900

    day 10 - 백준
    21318 문제 해결

--- 백준/Python/21318.py
+++ 백준/Python/21318.py
@@ -8,16 +8,22 @@ b = list(map(int, input().split()))
 # 질문의 개수
 c = int(input())
 
-d = [0] * c
+# 결과 (누적) 저장을 위한 리스트 초기화 진행 
+# 악보 간의 실수 횟수 (누적)
+d = [0] * int(a + 1)
+tmp = 0
 
-# 악보 실수 횟수 계산 (미리해야..)
-for i in range(1, c):
+# 악보 실수 횟수 계산 (누적합 계산)
+# tmp -> 현재까지 발견한 실수 횟수 저장 / 임시변수
+for i in range(a-1):
     if b[i] > b[i+1]:
-        d[i] += 1
-print(d)
+        tmp += 1
+    d[i+1] = tmp
+d[-1] = tmp
 
-# 질문에 따른 출력 
+# print(d)
 
-#for i in range(c):
-#    x, y = map(int, input().split())
-#    print(d[])
+# 질문에 따른 출력 / 구간 사이에 발생한 실수 횟수 계산 
+for i in range(c):
+    x, y = map(int, input().split())
+    print(d[y-1] - d[x-1])
