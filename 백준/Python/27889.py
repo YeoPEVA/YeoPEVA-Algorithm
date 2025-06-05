a0eb2595c9c633dfb1f9974ffe0c4867f5faa78c
YeoPEVA <lcy606@naver.com>
2025-06-05 12:41:23 +0900

    day 66 - 백준
    66일차 풀이 내용

---
+++ 백준/Python/27889.py
@@ -0,0 +1,11 @@
+schoolN = input()
+
+if schoolN == "NLCS":
+    print("North London Collegiate School")
+elif schoolN == "BHA":
+    print("Branksome Hall Asia")
+elif schoolN == "KIS":
+    print("Korea International School")
+else:
+    print("St. Johnsbury Academy")
+
