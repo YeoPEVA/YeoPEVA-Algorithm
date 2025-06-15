e456553447870978d8904a7d03cbe76cdf4a8c94
YeoPEVA <lcy606@naver.com>
2025-06-15 17:44:02 +0900

    day 76 - 백준
    76일차 풀이 내용

---
+++ 백준/Python/5554.py
@@ -0,0 +1,9 @@
+HometoS = int(input())
+HometoP = int(input())
+PtoC = int(input())
+CtoHome = int(input())
+
+result = HometoS + HometoP + PtoC + CtoHome
+
+print(int(result/60))
+print(int(result%60))
