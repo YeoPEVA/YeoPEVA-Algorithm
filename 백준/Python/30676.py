4767f3c704860f68d1ff9e8c2defa7a3f6d11f2e
YeoPEVA <lcy606@naver.com>
2025-07-10 17:26:16 +0900

    day 101 - 백준
    101일차 풀이 내용

---
+++ 백준/Python/30676.py
@@ -0,0 +1,16 @@
+p = int(input())
+
+if p >= 620 and 780 >= p:
+    print("Red")
+elif p >= 590 and 620 > p:
+    print("Orange")
+elif p >= 570 and 590 > p:
+    print("Yellow")
+elif p >= 495 and 570 > p:
+    print("Green")
+elif p >= 450 and 495 > p:
+    print("Blue")
+elif p >= 425 and 450 > p:
+    print("Indigo")
+else:
+    print("Violet")
