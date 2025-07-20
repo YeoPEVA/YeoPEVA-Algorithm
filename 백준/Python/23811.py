931d3edf0274bddd3c13aebd7d0c71fabd328485
YeoPEVA <lcy606@naver.com>
2025-07-20 17:22:39 +0900

    day 111 - 백준
    111일차 풀이 내용

---
+++ 백준/Python/23811.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    print('@' * (5*n))
+
+for i in range(n):
+    print('@' * n)
+
+for i in range(n):
+    print('@'*(5*n))
+
+for i in range(n):
+    print('@'*n)
+
+for i in range(n):
+    print('@' * (5*n))
